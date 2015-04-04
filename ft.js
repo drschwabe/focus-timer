a04e5553cddbebd488217ed2f2ab881cd1a0a5eb
Derrick Schwabe <admin@nerdbusiness.com>
2015-04-04 15:02:50 -0600

    -Replace spinner with a progress bar. -Provide an end time.

--- ft.js
+++ ft.js
@@ -1,47 +1,36 @@
 var argv = require('minimist')(process.argv.slice(2)),
     _ = require('underscore'),
-    cli = require('cli')
+    ProgressBar = require('progress')
 
-console.log('\n### Focus Timer ###');
+console.log('\n### Focus Timer ###')
 
 Date.prototype.now = function () {
-   return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes();
+   return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes()
 }
 
 var ft = {}
 
 ft.start = function(minutes) {
 
-  var newTime = new Date()
-    console.log('-- %s minute session (%s start) --', minutes, newTime.now())
+  var startTime = new Date()
+    console.log('%s minute session (%s start)', minutes, startTime.now())
 
-  cli.spinner('focusing');
+  var milliseconds = minutes * 60000,
+      seconds = minutes * 60
 
-  //Convert minutes to milliseconds:
-  var milliseconds = minutes * 60000
-
-  //For displaying progress:
-  var minutesFocused = 0
-  var spinnerInterval = setInterval(function() {
-    minutesFocused = minutesFocused + 1
-    console.log(' (' + minutesFocused + ' minutes so far) ')
-  }, 60000)
-
-  //End timer:
-  setTimeout(function() {
-    clearInterval(spinnerInterval);
-    console.log(' (' + minutes + ' minutes total)');
-    console.log('\007'); //< Sound effect.
-    cli.spinner('focus session complete!\n\n', true); //End the spinner
-  }, milliseconds)
+  //Progress bar:
+  var bar = new ProgressBar(':bar', { total: seconds })
+  var timer = setInterval(function () {
+    bar.tick()
+    if (bar.complete) {
+      var endTime = new Date()
+      console.log('focus session complete! (%s end)', endTime.now())
+      console.log('\007') //< Sound effect.
+      clearInterval(timer);
+    }
+  }, 1000)
 }
 
-
-
 //Parse user issued command from terminal:
-if( _.isEmpty(argv._) || !ft[argv._[0]]) return;
-//Execute the corresponding function with arguments, if any:
-ft[ argv._[0] ]( argv._[1], argv._[2], argv._[3], argv._[4] )
-
-
-
+if( _.isEmpty(argv._) || !ft[argv._[0]]) return
+ft[ argv._[0] ]( argv._[1] )
